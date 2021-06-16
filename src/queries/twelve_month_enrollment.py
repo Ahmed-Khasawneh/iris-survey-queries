fbd8fcc14ed8cd190dcf5b241c0be2f75d6c70c3
Your Name <ahmed.khasawneh@evisions.com>
2021-06-16 15:32:03 -0700

    changes to buildspec.yml

--- src/queries/twelve_month_enrollment.py
+++ src/queries/twelve_month_enrollment.py
@@ -2,30 +2,53 @@ from pyspark import SparkContext
 from awsglue.context import GlueContext
 from pyspark.sql import SQLContext, types as T, functions as f
 from pyspark.sql.functions import sum as _sum, expr, col, lit
-import lib.query_helpers as helpers
-import utilities.s3_utility as s3_utility
+from awsglue.utils import getResolvedOptions
+import query_helpers
+#import numpy as np
 import pandas as pd
+#from script_1 import df # look into importing a dataframe from script_1 to script_2
 
 glueContext = GlueContext(SparkContext.getOrCreate())
 sparkContext = SparkContext.getOrCreate()
 spark = SQLContext(sparkContext)
 
-#Pass in the survey run values
+optionNames = [
+    'survey_type',
+    'survey_year'
+]
+
+args = getResolvedOptions(sys.argv, optionNames)
+
 #Default survey values
-var_surveyYear = '1415'
-var_surveyId = 'E1D'
-var_surveyVersion = '1'
+var_surveyYear = args['survey_year']
 
-global_ipedsClientConfig = spark.sql(helpers.func_ipedsClientConfig(surveyYear = var_surveyYear)).limit(1).collect()
+survey_id_map = {
+    'TWELEVE_MONTH_ENROLLMENT_1': 'E1D', 
+    'TWELEVE_MONTH_ENROLLMENT_2': 'E12',
+    'TWELEVE_MONTH_ENROLLMENT_3': 'E1E',
+    'TWELEVE_MONTH_ENROLLMENT_4': 'E1F'
+}
+
+var_surveyId = survey_id_map[args['survey_type']]
+var_surveyType = '12ME'
+var_repEndTag = 'June End'
+
+#IPEDSReportingPeriod   
+global_ipedsReportingPeriod = spark.sql(func_ipedsReportingPeriod(surveyYear = var_surveyYear, surveyId = var_surveyId, repPeriodTag1 = var_repEndTag)).distinct()
+#(surveyYear = "1920", surveyId = 'EF1', repPeriodTag1 = '', repPeriodTag2 = '', repPeriodTag3 = '', repPeriodTag4 = '', repPeriodTag5 = '', dmVersion = '')
+
+reportEndSnapshot = pd.DataFrame(global_ipedsReportingPeriod.limit(1).select("snapshotDate").collect())
+reportEndSnapshot = reportEndSnapshot.to_string(header=False, index=False, index_names=False)
+
+#IPEDSClientConfig
+global_ipedsClientConfig = spark.sql(func_ipedsClientConfig(surveyYear = var_surveyYear, snapshotDate = reportEndSnapshot)).limit(1).collect()
 #(surveyYear = "1920", snapshotDate = "9999-09-09", dmVersion = '')
+
 global_ipedsClientConfig = pd.DataFrame(global_ipedsClientConfig)
+global_ipedsClientConfig = global_ipedsClientConfig.to_string(header=False, index=False, index_names=False).split()
+list_ipedsClientConfig = ["', '".join(ele.split()) for ele in global_ipedsClientConfig]
 
-dfToStr = global_ipedsClientConfig.to_string(
-        header=False,
-        index=False,
-        index_names=False).split()
-list_ipedsClientConfig = ["', '".join(ele.split()) for ele in dfToStr]
-
+config_acadOrProgReporter = list_ipedsClientConfig[0]
 config_genderForNonBinary = list_ipedsClientConfig[30]
 config_genderForUnknown = list_ipedsClientConfig[31]
 config_icOfferDoctorAwardLevel = list_ipedsClientConfig[34]
@@ -34,110 +57,91 @@ config_icOfferUndergradAwardLevel = list_ipedsClientConfig[36]
 config_instructionalActivityType = list_ipedsClientConfig[38]
 config_tmAnnualDPPCreditHoursFTE = list_ipedsClientConfig[51]
 
-#IPEDSReportingPeriod   
-global_ipedsReportingPeriod = spark.sql(helpers.func_ipedsReportingPeriod(surveyYear = var_surveyYear, surveyId = var_surveyId, repPeriodTag1 = var_repPeriodTag1)).distinct()
-#(surveyYear = "1920", surveyId = 'EF1', repPeriodTag1 = '', repPeriodTag2 = '', repPeriodTag3 = '', repPeriodTag4 = '', repPeriodTag5 = '', dmVersion = '')
-
 #AcademicTerm
-global_academicTerm = spark.sql(helpers.func_academicTerm())
+global_academicTerm = spark.sql(func_academicTerm())
+#(repPeriodTag1 = '', repPeriodTag2 = '', repPeriodTag3 = '', repPeriodTag4 = '', repPeriodTag5 = '', dmVersion = '')
 
 #IPEDSReportingPeriod || AcademicTerm
-global_ipedsReportingPeriod = global_ipedsReportingPeriod.join(
+global_reportingPeriodOrder = global_ipedsReportingPeriod.join(
     global_academicTerm, 
     (global_ipedsReportingPeriod.termCode == global_academicTerm.termCode) &
     (global_ipedsReportingPeriod.partOfTermCode == global_academicTerm.partOfTermCode), 'inner').select(
     global_ipedsReportingPeriod["*"], 
     global_academicTerm.termOrder,
     global_academicTerm.termType,
+    global_academicTerm.termClassification,
+    global_academicTerm.financialAidYear,
     global_academicTerm.censusDate,
+    global_academicTerm.maxCensus,
+    global_academicTerm.startDate,
+    global_academicTerm.endDate,
     global_academicTerm.requiredFTClockHoursUG,
     global_academicTerm.requiredFTCreditHoursGR, 
     global_academicTerm.requiredFTCreditHoursUG)
 
-global_ipedsReportingPeriod.createOrReplaceTempView("global_ipedsReportingPeriod")
+global_reportingPeriodOrder.createOrReplaceTempView("global_reportingPeriodOrder")
+
+global_reportingPeriodRefactor = spark.sql(func_reportingPeriodRefactor()).distinct()
+        
+global_reportingPeriodRefactor.createOrReplaceTempView("global_reportingPeriodRefactor")
 
 #Cohort
-#Replace 'cohort' df with this function call 
-#global_cohort = spark.sql(helpers.func_cohort(
-#    df_cohort = "global_ipedsReportingPeriod", 
-#    genderForNonBinary = config_genderForNonBinary,
-#    genderForUnknown = config_genderForUnknown,
-#    instructionalActivityType = config_instructionalActivityType
-#    ))
-columns = ["personId", "termCode", "partOfTermCode", "studentLevel", "studentType", "residency", "ipedsGender", "ipedsEthnicity", "timeStatus", "isNonDegreeSeeking", "age", "enrollmentHours", "isClockHours", "DEStatus"]
-data = [("1111", "201410", "1", "Masters", "First Time", "" , "M" , "1" , "FT" , "false" , "100", "12", "false", "DE Some"), 
-        ("1111", "201430", "1", "Masters","First Time", "" , "M" , "1" , "FT" , "false" , "100", "12", "false", "DE Some"),
-        ("2222", "201410", "1", "Masters", "First Time", "" , "M" , "1" , "FT" , "false" , "100", "12", "false", "DE Some"), 
-        ("2222", "201430", "1", "Masters", "First Time", "" , "M" , "1" , "FT" , "false" , "100", "12", "false", "DE Some"),
-        ("3333", "201410", "1", "Masters", "First Time", "" , "F" , "1" , "FT" , "false" , "100", "12", "false", "DE Some"), 
-        ("3333", "201430", "1", "Masters", "First Time", "" , "F" , "1" , "FT" , "false" , "100", "12", "false", "DE Some"),
-        ("4444", "201410", "1", "Masters", "First Time", "" , "F" , "1" , "FT" , "false" , "100", "12", "false", "DE Some"), 
-        ("4444", "201430", "1", "Masters", "First Time", "" , "F" , "1" , "FT" , "false" , "100", "12", "false", "DE Some")]
+global_cohort = spark.sql(func_cohort(repPeriod = 'global_reportingPeriodRefactor', termOrder = 'global_reportingPeriodOrder', instructionalActivityType = config_instructionalActivityType, genderForNonBinary = config_genderForNonBinary, genderForUnknown = config_genderForUnknown, acadOrProgReporter = config_acadOrProgReporter, surveyType = var_surveyType))
 
-cohort = sparkContext.parallelize(data)
-cohort = spark.createDataFrame(cohort).toDF(*columns)
+#CourseLevelCounts
+global_courseLevelCounts = spark.sql(func_courseLevelCounts(repPeriod = 'global_reportingPeriodRefactor', termOrder = 'global_reportingPeriodOrder', instructionalActivityType = config_instructionalActivityType))
+global_courseLevelCounts = global_courseLevelCounts.join(
+    global_cohort,
+    (global_cohort.personId == global_courseLevelCounts.personId), 'inner').select(
+        global_courseLevelCounts["*"],
+        global_cohort.ipedsEnrolled).filter(global_cohort.ipedsEnrolled == 1)
 
-cohort = cohort.join(
-    global_academicTerm,
-    (global_academicTerm.termCode == cohort.termCode) &
-    (global_academicTerm.partOfTermCode == cohort.partOfTermCode), "inner").select(
-        cohort["*"])
-        
-CourseTypeCountsCRN = cohort.select(
-    #cohort["*"],
-    expr("case when isClockHours = 'false' and studentLevel in ('Undergraduate', 'Continuing Education', 'Other') then coalesce(enrollmentHours, 0) else 0 end").cast('int').alias("UGCreditHours"),
-    expr("case when isClockHours = 'true' and studentLevel in ('Undergraduate', 'Continuing Education', 'Other') then coalesce(enrollmentHours, 0) else 0 end").cast('int').alias("UGClockHours"),
-    expr("case when studentLevel in ('Masters', 'Doctorate') then coalesce(enrollmentHours, 0) else 0 end").cast('int').alias("GRCreditHours"),
-    expr("case when studentLevel in ('Professional Practice Doctorate') then coalesce(enrollmentHours, 0) else 0 end").cast('int').alias("DPPCreditHours"))
-
-CourseTypeCountsCRN = CourseTypeCountsCRN.agg(
+CourseTypeCountsCRN = global_courseLevelCounts.agg(
     _sum("UGCreditHours").alias("UGCreditHours"),
     _sum("UGClockHours").alias("UGClockHours"),
     _sum("GRCreditHours").alias("GRCreditHours"),
     _sum("DPPCreditHours").alias("DPPCreditHours"))
 
-cohort_out = cohort.select(
-    cohort["*"],
+cohort_out = global_cohort.select(
+    global_cohort["*"],
     expr("""
-    case when studentLevel in ('Masters', 'Doctorate') then '99' 
-         when isNonDegreeSeeking = true and timeStatus = 'FT' then '7'
-         when isNonDegreeSeeking = true and timeStatus = 'PT' then '21'
-         when studentLevel in ('Undergraduate', 'Continuing Education', 'Other') then 
-            (case when studentType = 'First Time' and timeStatus = 'FT' then '1' 
-                    when studentType = 'Transfer' and timeStatus = 'FT' then '2'
-                    when studentType = 'Returning' and timeStatus = 'FT' then '3'
-                    when studentType = 'First Time' and timeStatus = 'PT' then '15' 
-                    when studentType = 'Transfer' and timeStatus = 'PT' then '16'
-                    when studentType = 'Returning' and timeStatus = 'PT' then '17' else '1' 
-             end)
-        else null
+    case when studentLevelUGGR = 'GR' then '99'
+         when isNonDegreeSeeking = 1 and timeStatus = 'Full Time' then '7'
+         when isNonDegreeSeeking = 1 and timeStatus = 'Part Time' then '21'
+         when studentType = 'First Time' and timeStatus = 'Full Time' then '1'
+         when studentType = 'Transfer' and timeStatus = 'Full Time' then '2'
+         when studentType = 'Continuing' and timeStatus = 'Full Time' then '3'
+         when studentType = 'First Time' and timeStatus = 'Part Time' then '15'
+         when studentType = 'Transfer' and timeStatus = 'Part Time' then '16'
+         when studentType = 'Continuing' and timeStatus = 'Part Time' then '17'
+         else '1'
     end
     """).alias("ipedsPartAStudentLevel"),
     expr("""
-    case when studentLevel in ('Masters', 'Doctorate') then '3' 
-         when isNonDegreeSeeking = true then '2'
-         when studentLevel in ('Undergraduate', 'Continuing Education', 'Other') then '1'
+    case when studentLevelUGGR = 'GR' then '3'
+         when isNonDegreeSeeking = 1 then '2'
+         when studentLevelUGGR = 'UG' then '1'
          else null
     end
     """).alias("ipedsPartCStudentLevel")
-    )
-
+    ).filter(global_cohort.ipedsEnrolled == 1)
+    
 #Survey version output lists
-if var_surveyVersion == '1':
+if var_surveyId == 'E1D':
     A_UgGrBoth = ["1", "2", "3", "7", "15", "16", "17", "21", "99"]
     A_UgOnly = ["1", "2", "3", "7", "15", "16", "17", "21"]
     A_GrOnly = ["99"]
     C_UgGrBoth = ["1", "2", "3"]
     C_UgOnly = ["1", "2"]
     C_GrOnly = ["3"]
-elif var_surveyVersion == '2':
+elif var_surveyId == 'E12':
     A_UgGrBoth = ["1", "2", "3", "7", "15", "16", "17", "21"]
     A_UgOnly = ["1", "2", "3", "7", "15", "16", "17", "21"]
     A_GrOnly = [""] 
     C_UgGrBoth = ["1", "2"]
     C_UgOnly = ["1", "2"]
     C_GrOnly = [""]
-elif var_surveyVersion == '3':
+elif var_surveyId == 'E1E':
     A_UgGrBoth = ["1", "3", "7", "15", "17", "21"]
     A_UgOnly = ["1", "3", "7", "15", "17", "21"]
     A_GrOnly = [""] 
@@ -151,7 +155,7 @@ else: #V4
     C_UgGrBoth = ["1", "2", "3"]
     C_UgOnly = ["1", "2"]
     C_GrOnly = ["3"]
-    
+
 #Part A
 if cohort_out.rdd.isEmpty() == False:
     #FormatPartA
@@ -224,21 +228,21 @@ elif config_icOfferUndergradAwardLevel == 'Y' and config_icOfferGraduateAwardLev
     partA_out = partA_out.where(partA_out.field1.isin(A_UgOnly))
 elif config_icOfferUndergradAwardLevel == 'N' and config_icOfferGraduateAwardLevel == 'Y':
     partA_out = partA_out.where(partA_out.field1.isin(A_GrOnly))
-
+    
 #Part C
 if cohort_out.rdd.isEmpty() == False:
 	#FormatPartC
-	c_columns = ["personId", "ipedsLevel", "DEStatus"]
+	c_columns = ["personId", "ipedsLevel", "distanceEdInd"]
 	c_data = [("", "1", ""), ("", "2", ""), ("", "3", "")]
 	FormatPartC = sparkContext.parallelize(c_data)
 	FormatPartC = spark.createDataFrame(FormatPartC).toDF(*c_columns)
 
 	#Part C
-	partC_out = cohort_out.select("personId", "ipedsPartCStudentLevel", "DEStatus").filter((cohort_out.ipedsPartCStudentLevel.isNotNull()) & (cohort_out.ipedsPartCStudentLevel != '') & (cohort_out.DEStatus != 'DE None')).union(FormatPartC)
+	partC_out = cohort_out.select("personId", "ipedsPartCStudentLevel", "distanceEdInd").filter((cohort_out.ipedsPartCStudentLevel.isNotNull()) & (cohort_out.ipedsPartCStudentLevel != '') & (cohort_out.distanceEdInd != 'None')).union(FormatPartC)
 	partC_out = partC_out.select(
 		partC_out.ipedsPartCStudentLevel.alias("field1"),
-		expr("case when DEStatus = 'DE Exclusively' then 1 else 0 end").cast('int').alias("field2"), # Enrolled exclusively in distance education courses
-		expr("case when DEStatus = 'DE Some' then 1 else 0 end").cast('int').alias("field3"))        # Enrolled in at least one but not all distance education courses
+		expr("case when distanceEdInd = 'Exclusive DE' then 1 else 0 end").cast('int').alias("field2"), # Enrolled exclusively in distance education courses
+		expr("case when distanceEdInd = 'Some DE' then 1 else 0 end").cast('int').alias("field3"))        # Enrolled in at least one but not all distance education courses
 
 	partC_out = partC_out.withColumn('part', f.lit('C')).groupBy("part", "field1").agg(
 		_sum("field2").alias("field2"),
@@ -267,19 +271,18 @@ if CourseTypeCountsCRN.rdd.isEmpty() == False:
     # CONTHRS  - clock hour instructional activity at the undergraduate level, 0 to 9999999, blank = not applicable, if no undergraduate programs are measured in clock hours.
 	    expr(f"round((case when '{config_icOfferUndergradAwardLevel}' = 'Y' and '{config_instructionalActivityType}' != 'CR' then coalesce(UGClockHours, 0) else null end))").cast('int').alias("field3"),
     # CREDHRSG - credit hour instructional activity at the graduate level, 0 to 99999999, blank = not applicable
-	    expr(f"round((case when '{config_icOfferGraduateAwardLevel}' = 'Y' and '{var_surveyVersion}' = '1' then coalesce(GRCreditHours, 0) else null end))").cast('int').alias("field4"),
+	    expr(f"round((case when '{config_icOfferGraduateAwardLevel}' = 'Y' and '{var_surveyId}' = 'E1D' then coalesce(GRCreditHours, 0) else null end))").cast('int').alias("field4"),
     # RDOCFTE  - reported Doctor'92s degree-professional practice student FTE, 0 to 99999999, blank = not applicable
-	    expr(f"""round((case when '{config_icOfferDoctorAwardLevel}' = 'Y' and '{var_surveyVersion}' = '1' then 
+	    expr(f"""round((case when '{config_icOfferDoctorAwardLevel}' = 'Y' and '{var_surveyId}' = 'E1D' then 
 						    (case when coalesce(DPPCreditHours, 0) > 0 then coalesce(cast(round(DPPCreditHours / {config_tmAnnualDPPCreditHoursFTE}, 0) as string), '0') 
 					    else '0' end) else null end))""").cast('int').alias("field5")
-    #partB_out = partB_out.withColumn('part', f.lit('B'))
 	)
 else:
     b_columns = ["part", "field2", "field3", "field4", "field5"]
     b_data = [("B", "0", "", "0", "0")]
     partB_out = sparkContext.parallelize(b_data)
     partB_out = spark.createDataFrame(partB_out).toDF(*b_columns)
-
+    
 #Survey out formatting
 for column in [column for column in partB_out.columns if column not in partA_out.columns]:
     partA_out = partA_out.withColumn(column, lit(None))
@@ -292,11 +295,9 @@ for column in [column for column in partA_out.columns if column not in partB_out
     
 surveyOutput = partA_out.unionByName(partC_out).unionByName(partB_out)
 
-surveyOutput = glue_utility.create_json_format(surveyOutput)
-s3_utility.write_dataframe_as_json_to_s3(surveyOutput.repartition(1), s3_path, constants.SPARK_OVERWRITE_MODE, 'json')
+surveyOutput.show()
+#surveyOutput = create_json_format(surveyOutput)
+#write_dataframe_as_json_to_s3(surveyOutput.repartition(1), s3_path, constants.SPARK_OVERWRITE_MODE, 'json')
 
+#global_courseLevelCounts.show()
 #surveyOutput.show()
-#partA_out.show()
-#partC_out.show()
-#partB_out.show()   
-#CourseTypeCountsCRN.printSchema()
