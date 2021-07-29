82fe9a918e8879a60fcb3bdbd0d176739fffdf08
Ahmed-Khasawneh <52083285+Ahmed-Khasawneh@users.noreply.github.com>
2021-07-29 09:31:32 -0700

    Testing

--- queries/twelve_month_enrollment_query.py
+++ queries/twelve_month_enrollment_query.py
@@ -182,7 +182,7 @@ academic_term_partition_filter = "coalesce(isIpedsReportable, true) = true"
 ipeds_client_config_out = query_helpers.ipeds_client_config_mcr(ipeds_client_config_partition, ipeds_client_config_order,
                             ipeds_client_config_partition_filter)
 
-academic_term = academic_term_mcr(
+academic_term = query_helpers.academic_term_mcr(
     academic_term_partition,
     academic_term_order,
     academic_term_partition_filter).cache()
