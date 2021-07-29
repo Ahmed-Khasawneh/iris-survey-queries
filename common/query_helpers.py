cf3965f639ff2015466b8c3126ff7219d137b6f9
Ahmed-Khasawneh <52083285+Ahmed-Khasawneh@users.noreply.github.com>
2021-07-29 11:30:11 -0700

    testing

--- common/query_helpers.py
+++ common/query_helpers.py
@@ -282,6 +282,8 @@ def academic_term_reporting_refactor(
     academic_term_order, 
     academic_term_partition_filter):
     
+    acadeemic_term = academic_term_mcr(academic_term_partition, academic_term_order, academic_term_partition_filter)
+    
     ipeds_reporting_period_in = spark.sql("select * from ipedsReportingPeriod")
 
     ipeds_reporting_period_2 = academic_term.join(broadcast(ipeds_reporting_period_in),
