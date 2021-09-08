b00ba2e3762b5e7b04d68736ecca3308e9852d35
Ahmed-Khasawneh <52083285+Ahmed-Khasawneh@users.noreply.github.com>
2021-09-08 14:49:52 -0700

    Update studentfinancialaid_v2.sql

--- sql/2021/studentfinancialaid/studentfinancialaid_v2.sql
+++ sql/2021/studentfinancialaid/studentfinancialaid_v2.sql
@@ -1,6 +1,6 @@
 /********************
 
-EVI PRODUCT:    DORIS 2020-21 IPEDS Survey Winter Collection
+EVI PRODUCT:    DORIS 2021-22 IPEDS Survey Winter Collection
 FILE NAME:      Student Financial Aid v2 (SFA)
 FILE DESC:      Student Financial Aid for private institutions reporting on a fall cohort (academic reporters)
 AUTHOR:         Ahmed Khasawneh
