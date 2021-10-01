56c2fee3f26f2c6a94a5dc2fe64db91c3ac6d887
jhanicak <31990830+jhanicak@users.noreply.github.com>
2021-10-01 17:59:13 -0500

    Update default_values.py

--- common/default_values.py
+++ common/default_values.py
@@ -13,183 +13,192 @@ from awsglue.context import GlueContext
 from pyspark.sql import SQLContext, types as T, functions as f, SparkSession
 from awsglue.utils import getResolvedOptions
 
+def get_12me_dict(survey_ver_id, year_in, default_dictionary_in):
+    
+#E1D 12-month Enrollment for 4-year degree-granting institutions
+#E12 12-month Enrollment for 2-year degree-granting institutions        
+#E1E 12-month Enrollment for public 2-year and less-than-2-year non-degree-granting institutions
+#E1F 12-month Enrollment for private 2-year and less-than-2-year non-degree-granting institutions
+
+    year = year_in
+    next_year = str(int(year) + 1) 
+    prior_year = str(int(year) - 1) 
+
+    default_dictionary_in['report_start'] = datetime.strftime(datetime.strptime((prior_year + '-07-01'), "%Y-%m-%d"), "%Y-%m-%d")
+    default_dictionary_in['report_end'] = datetime.strftime(datetime.strptime((year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
+    default_dictionary_in['cohort_academic_fall'] = 'Fall Census'
+    default_dictionary_in['cohort_academic_pre_fall_summer'] = 'Pre-Fall Summer Census'
+    default_dictionary_in['cohort_academic_spring_tag'] = 'Spring Census'
+    default_dictionary_in['cohort_academic_post_spring_summer_tag'] = 'Post-Spring Summer Census'
+    default_dictionary_in['cohort_program'] = 'Academic Year End'
+    default_dictionary_in['cohort_program_2'] = 'June End'
+    default_dictionary_in['current_survey_sections'] = ['COHORT']
+        
+    return default_dictionary_in
+
+def get_gr_dict(survey_ver_id, year_in, default_dictionary_in):
+
+#GR1 Graduation Rates for 4-year institutions reporting on a fall cohort (academic reporters)
+#GR2 Graduation Rates for 4-year institutions reporting on a full-year cohort (program reporters)
+#GR3 Graduation Rates for 2-year institutions reporting on a fall cohort (academic reporters)
+#GR4 Graduation Rates for 2-year institutions reporting on a full-year cohort (program reporters)
+#GR5 Graduation Rates for less-than-2-year institutions reporting on a fall cohort (academic reporters)
+#GR6 Graduation Rates for less-than-2-year institutions reporting on a full-year cohort (program reporters)
+
+    year = year_in
+    next_year = str(int(year) + 1) 
+    prior_year = str(int(year) - 1)
+    prior_year_2 = str(int(year) - 2) 
+    gr_4yr_cohort_year = str(int(year) - 6)
+    gr_4yr_cohort_next_year = str(int(year) - 5)
+    gr_2yr_cohort_year = str(int(year) - 3)
+    gr_2yr_cohort_next_year = prior_year_2
+    
+    default_dictionary_in['financial_aid'] = 'Financial Aid Year End'
+    default_dictionary_in['transfer'] = 'Student Transfer Data'
+    default_dictionary_in['latest_or_only_status'] = 'August End'
+    default_dictionary_in['latest_or_only_status_as_of'] = datetime.strftime(datetime.strptime((year + '-08-30'), "%Y-%m-%d"), "%Y-%m-%d")
+    default_dictionary_in['current_survey_sections'] = ['COHORT', 'PRIOR SUMMER']   #['FALL', 'COHORT', 'PRIOR SUMMER']  
+    
+    if survey_ver_id == 'GR1':
+        default_dictionary_in['cohort_academic_fall'] = 'Fall Census'
+        default_dictionary_in['default_fall_census'] = datetime.strftime(datetime.strptime((gr_4yr_cohort_year + '-10-15'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['cohort_academic_pre_fall_summer'] = 'Pre-Fall Summer Census'
+        default_dictionary_in['earliest_or_only_status'] = 'August End'
+        default_dictionary_in['earliest_or_only_status_as_of'] = datetime.strftime(datetime.strptime((prior_year_2 + '-08-30'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['mid_or_only_status'] = 'August End'
+        default_dictionary_in['mid_or_only_status_as_of'] = datetime.strftime(datetime.strptime((prior_year + '-08-30'), "%Y-%m-%d"), "%Y-%m-%d")
+
+    elif survey_ver_id == 'GR2':        
+        default_dictionary_in['cohort_program'] = 'August End'
+        default_dictionary_in['cohort_program_start'] = datetime.strftime(datetime.strptime((gr_4yr_cohort_year + '-09-01'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['cohort_program_end'] = datetime.strftime(datetime.strptime((gr_4yr_cohort_next_year + '-08-31'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['earliest_or_only_status'] = 'August End'
+        default_dictionary_in['earliest_or_only_status_as_of'] = datetime.strftime(datetime.strptime((prior_year_2 + '-08-30'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['mid_or_only_status'] = 'August End'
+        default_dictionary_in['mid_or_only_status_as_of'] = datetime.strftime(datetime.strptime((prior_year + '-08-30'), "%Y-%m-%d"), "%Y-%m-%d")
+        
+    elif survey_ver_id in ('GR3', 'GR5'):
+        default_dictionary_in['cohort_academic_fall'] = 'Fall Census'
+        default_dictionary_in['default_fall_census'] = datetime.strftime(datetime.strptime((gr_2yr_cohort_year + '-10-15'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['cohort_academic_pre_fall_summer'] = 'Pre-Fall Summer Census'
+        
+    else: # survey_ver_id in ('GR4', 'GR6'):        
+        default_dictionary_in['cohort_program'] = 'August End'
+        default_dictionary_in['cohort_program_start'] = datetime.strftime(datetime.strptime((gr_2yr_cohort_year + '-09-01'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['cohort_program_end'] = datetime.strftime(datetime.strptime((gr_2yr_cohort_next_year + '-08-31'), "%Y-%m-%d"), "%Y-%m-%d")
+        
+    return default_dictionary_in
+    
+    
+def get_sfa_dict(survey_ver_id, year_in, default_dictionary_in):
+
+#SFA1 Student Financial Aid for public institutions reporting on a fall cohort (academic reporters)
+#SFA2 Student Financial Aid for private institutions reporting on a fall cohort (academic reporters)
+#SFA3 Student Financial Aid for institutions reporting on a full-year cohort (public program reporters)
+#SFA4 Student Financial Aid for institutions reporting on a full-year cohort (private program reporters)
+#SFA5 Student Financial Aid for institutions with graduate students only
+
+    year = year_in
+    next_year = str(int(year) + 1) 
+    prior_year = str(int(year) - 1) 
+    
+    default_dictionary_in['financial_aid'] = 'Financial Aid Year End'
+    default_dictionary_in['gi_bill'] = 'GI Bill'
+    default_dictionary_in['gi_bill_start'] = datetime.strftime(datetime.strptime((prior_year + '-07-01'), "%Y-%m-%d"), "%Y-%m-%d")
+    default_dictionary_in['gi_bill_end'] = datetime.strftime(datetime.strptime((year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
+    default_dictionary_in['department_of_defense'] = 'Department of Defense'
+    default_dictionary_in['department_of_defense_start'] = datetime.strftime(datetime.strptime((prior_year + '-10-01'), "%Y-%m-%d"), "%Y-%m-%d")
+    default_dictionary_in['department_of_defense_end'] = datetime.strftime(datetime.strptime((year + '-09-30'), "%Y-%m-%d"), "%Y-%m-%d")
+    default_dictionary_in['current_survey_sections'] = ['COHORT', 'PRIOR SUMMER']   #['FALL', 'COHORT', 'PRIOR SUMMER']  
+    default_dictionary_in['prior_survey_sections'] = ['PRIOR YEAR 1 COHORT', 'PRIOR YEAR 1 PRIOR SUMMER']
+    default_dictionary_in['prior_2_survey_sections'] = ['PRIOR YEAR 2 COHORT', 'PRIOR YEAR 2 PRIOR SUMMER']
+    
+    if survey_ver_id in ('SFA1', 'SFA2'):
+        default_dictionary_in['cohort_academic_fall'] = 'Fall Census'
+        default_dictionary_in['default_fall_census'] = datetime.strftime(datetime.strptime((prior_year + '-10-15'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['cohort_academic_pre_fall_summer'] = 'Pre-Fall Summer Census'
+        default_dictionary_in['cohort_hybrid'] = 'October End'
+        default_dictionary_in['cohort_hybrid_start'] = datetime.strftime(datetime.strptime((prior_year + '-10-01'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['cohort_hybrid_end'] = datetime.strftime(datetime.strptime((prior_year + '-10-31'), "%Y-%m-%d"), "%Y-%m-%d")
+        
+    elif survey_ver_id in ('SFA3', 'SFA4'):
+        default_dictionary_in['financial_aid_start'] = datetime.strftime(datetime.strptime((year + '-07-01'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['financial_aid_end'] = datetime.strftime(datetime.strptime((next_year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['cohort_program'] = 'Academic Year End'
+        default_dictionary_in['cohort_program_start'] = datetime.strftime(datetime.strptime((prior_year + '-10-01'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['cohort_program_end'] = datetime.strftime(datetime.strptime((prior_year + '-10-31'), "%Y-%m-%d"), "%Y-%m-%d")
+        
+    else:  # V5
+        default_dictionary_in['financial_aid'] = ''
+        
+    return default_dictionary_in
+
+    
+def get_updated_dictionary(survey_info_in, default_dictionary_in):
+    
+    survey_type = survey_info_in['survey_type']
+    survey_ver_id = survey_info_in['survey_ver_id']
+    year = survey_info_in['survey_year_iris']  
+    
+    if survey_type == 'SFA':
+        survey_default_values = get_sfa_dict(survey_ver_id, year, default_dictionary_in)
+    elif survey_type == '12ME':
+        survey_default_values = get_12me_dict(survey_ver_id, year, default_dictionary_in)
+    else: # survey_type == 'GR':
+        survey_default_values = get_gr_dict(survey_ver_id, year, default_dictionary_in)
+        
+    return survey_default_values
+    
+    
 #***************************************************************
 #*
 #***  get_survey_tags 
 #*
 #***************************************************************
 
-def get_survey_tags(survey_info_in):
-    
-    survey_year = survey_info_in['survey_year_doris']
-    survey_ver_id = survey_info_in['survey_ver_id']
-    survey_type = survey_info_in['survey_type']
-    survey_id = survey_info_in['survey_id']
-    year = survey_info_in['survey_year_iris']
-    
-    cohort_academic_fall_tag = 'Fall Census'
-    cohort_academic_pre_fall_summer_tag = 'Pre-Fall Summer Census'
-    cohort_academic_spring_tag = 'Spring Census'
-    cohort_academic_post_spring_summer_tag = 'Post-Spring Summer Census'
-    cohort_hybrid_tag = 'October End'
-    cohort_program_tag = 'Academic Year End'
-    gi_bill_tag = 'GI Bill'
-    department_of_defense_tag = 'Department of Defense'
-    financial_aid_tag = 'Financial Aid Year End'
-    cohort_status_tag_1 = 'August End'
-    cohort_status_tag_2 = 'Academic Year End'
-    transfer_tag = 'Student Transfer Data'
-    fiscal_year_tag = 'Fiscal Year Lockdown'
-    hr_tag = 'HR Reporting End'
+def get_survey_default_values(survey_info_in):
 
-    current_survey_sections = ['COHORT', 'PRIOR SUMMER']    
-####****TEST uncomment to test older IPEDSReportingPeriod snapshots
-    #current_survey_sections = ['FALL', 'COHORT', 'PRIOR SUMMER']
-    prior_survey_sections = ['PRIOR YEAR 1 COHORT', 'PRIOR YEAR 1 PRIOR SUMMER']
-    prior_2_survey_sections = ['PRIOR YEAR 2 COHORT', 'PRIOR YEAR 2 PRIOR SUMMER']
-
-    if survey_type == 'SFA':
-        cohort_academic_spring_tag = ''
-        cohort_academic_post_spring_summer_tag = ''
-        cohort_status_tag_1 = ''
-        cohort_status_tag_2 = ''
-        transfer_tag = ''
-        fiscal_year_tag = ''
-        hr_tag = ''
- 
-        if survey_ver_id == 'SFA1':
-            cohort_program_tag = ''
-        elif survey_ver_id == 'SFA2':
-            cohort_program_tag = ''
-        elif survey_ver_id == 'SFA3':
-            cohort_academic_fall_tag = ''
-            cohort_academic_pre_fall_summer_tag = ''
-            cohort_hybrid_tag = ''
-        elif survey_ver_id == 'SFA4':
-            cohort_academic_fall_tag = ''
-            cohort_academic_pre_fall_summer_tag = ''
-            cohort_hybrid_tag = ''
-        else:  # V5
-            cohort_academic_fall_tag = ''
-            cohort_academic_pre_fall_summer_tag = ''
-            cohort_program_tag = ''
-            cohort_hybrid_tag = ''
-            financial_aid_tag = ''
-    
-    if survey_type == '12ME':
-        cohort_hybrid_tag = ''
-        gi_bill_tag = ''
-        department_of_defense_tag = ''
-        financial_aid_tag = ''
-        transfer_tag = ''
-        fiscal_year_tag = ''
-        hr_tag = ''
-        current_survey_sections = ['COHORT']
-        prior_survey_sections = ''
-        prior_2_survey_sections = ''
- 
-    tag_dictionary = {'cohort_academic_fall_tag' : cohort_academic_fall_tag,
-        'cohort_academic_pre_fall_summer_tag' : cohort_academic_pre_fall_summer_tag,
-        'cohort_academic_spring_tag' : cohort_academic_spring_tag,
-        'cohort_academic_post_spring_summer_tag' : cohort_academic_post_spring_summer_tag,
-        'cohort_hybrid_tag' : cohort_hybrid_tag,
-        'cohort_program_tag' : cohort_program_tag,
-        'gi_bill_tag' : gi_bill_tag,
-        'department_of_defense_tag' : department_of_defense_tag,
-        'financial_aid_tag' : financial_aid_tag,
-        'cohort_status_tag_1' : cohort_status_tag_1,
-        'cohort_status_tag_2' : cohort_status_tag_2,
-        'transfer_tag' : transfer_tag,
-        'fiscal_year_tag' : fiscal_year_tag,
-        'hr_tag' : hr_tag,
-        'current_survey_sections' : current_survey_sections,
-        'prior_survey_sections' : prior_survey_sections,
-        'prior_2_survey_sections' : prior_2_survey_sections}
- 
-    return tag_dictionary
-
-#***************************************************************
-#*
-#***  get_survey_dates  
-#*
-#***************************************************************
-
-def get_survey_dates(survey_info_in):
-
-    survey_year = survey_info_in['survey_year_doris']
-    survey_ver_id = survey_info_in['survey_ver_id']
-    survey_type = survey_info_in['survey_type']
-    survey_id = survey_info_in['survey_id']
-    year = survey_info_in['survey_year_iris'] 
-    
-    year2 = str(int(year) + 1) 
-    prior_year = str(int(year) - 1) 
-
-    report_start_date = datetime.strftime(datetime.strptime((prior_year + '-07-01'), "%Y-%m-%d"), "%Y-%m-%d")
-    report_end_date = datetime.strftime(datetime.strptime((year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
-    default_fall_census_date = datetime.strftime(datetime.strptime((prior_year + '-10-15'), "%Y-%m-%d"), "%Y-%m-%d")
-    cohort_program_or_hybrid_start_date = datetime.strftime(datetime.strptime((prior_year + '-10-01'), "%Y-%m-%d"), "%Y-%m-%d")
-    cohort_program_or_hybrid_end_date = datetime.strftime(datetime.strptime((prior_year + '-10-31'), "%Y-%m-%d"), "%Y-%m-%d")
-    gi_bill_start_date = datetime.strftime(datetime.strptime((prior_year + '-07-01'), "%Y-%m-%d"), "%Y-%m-%d")
-    gi_bill_end_date = datetime.strftime(datetime.strptime((year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
-    department_of_defense_start_date = datetime.strftime(datetime.strptime((prior_year + '-10-01'), "%Y-%m-%d"), "%Y-%m-%d")
-    department_of_defense_end_date = datetime.strftime(datetime.strptime((year + '-09-30'), "%Y-%m-%d"), "%Y-%m-%d")
-    financial_aid_start_date = datetime.strftime(datetime.strptime((year + '-07-01'), "%Y-%m-%d"), "%Y-%m-%d")
-    financial_aid_end_date = datetime.strftime(datetime.strptime((year2 + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
-    earliest_status_date = ''
-    mid_status_date = ''
-    latest_status_date = ''
-    fiscal_year_as_of_date = ''
-    hr_as_of_date = ''
-
-    if survey_type == 'SFA':
-        report_start_date = '' 
-        report_end_date = ''
-    
-        if survey_ver_id == 'SFA1':
-            financial_aid_start_date = ''
-            financial_aid_end_date = ''
-        elif survey_ver_id == 'SFA2':
-            financial_aid_start_date = ''
-            financial_aid_end_date = ''
-        elif survey_ver_id == 'SFA3':
-            cohort_program_or_hybrid_start_date = ''
-            cohort_program_or_hybrid_end_date = ''
-        elif survey_ver_id == 'SFA4':
-            cohort_program_or_hybrid_start_date = ''
-            cohort_program_or_hybrid_end_date = ''
-            default_fall_census_date = ''
-        else:  # V5
-            cohort_program_or_hybrid_start_date = ''
-            cohort_program_or_hybrid_end_date = ''
-            default_fall_census_date = ''
-
-    if survey_type == '12ME':
-        default_fall_census_date = ''
-        cohort_program_or_hybrid_start_date = ''
-        cohort_program_or_hybrid_end_date = ''
-        gi_bill_start_date = ''
-        gi_bill_end_date = ''
-        department_of_defense_start_date = ''
-        department_of_defense_end_date = ''
-        financial_aid_start_date = ''
-        financial_aid_end_date = ''
-            
-    dates_dictionary = {'report_start_date' : report_start_date,
-        'report_end_date' : report_end_date,
-        'default_fall_census_date' : default_fall_census_date,
-        'cohort_program_or_hybrid_start_date' : cohort_program_or_hybrid_start_date,
-        'cohort_program_or_hybrid_end_date' : cohort_program_or_hybrid_end_date,
-        'gi_bill_start_date' : gi_bill_start_date,
-        'gi_bill_end_date' : gi_bill_end_date,
-        'department_of_defense_start_date' : department_of_defense_start_date,
-        'department_of_defense_end_date' : department_of_defense_end_date,
-        'financial_aid_start_date' : financial_aid_start_date,
-        'financial_aid_end_date' : financial_aid_end_date,
-        'earliest_status_date' : earliest_status_date,
-        'mid_status_date' : mid_status_date,
-        'latest_status_date' : latest_status_date,
-        'fiscal_year_as_of_date' : fiscal_year_as_of_date,
-        'hr_as_of_date' : hr_as_of_date}
- 
-    return dates_dictionary
+    default_dictionary = dict.fromkeys(['report_start',
+        'report_end',
+        'cohort_academic_fall',
+        'default_fall_census',
+        'cohort_academic_pre_fall_summer',
+        'cohort_academic_spring',
+        'cohort_academic_post_spring_summer',
+        'cohort_hybrid',
+        'cohort_hybrid_start',
+        'cohort_hybrid_end',
+        'cohort_program',
+        'cohort_program_2', 
+        'cohort_program_start',
+        'cohort_program_end', 
+        'gi_bill',
+        'gi_bill_start', 
+        'gi_bill_end',
+        'department_of_defense', 
+        'department_of_defense_start', 
+        'department_of_defense_end', 
+        'financial_aid', 
+        'financial_aid_start', 
+        'financial_aid_end', 
+        'transfer',
+        'earliest_status', 
+        'earliest_status_as_of', 
+        'mid_status', 
+        'mid_status_as_of', 
+        'latest_or_only_status', 
+        'latest_or_only_status_as_of', 
+        'fiscal_year', 
+        'fiscal_year_as_of', 
+        'hr', 
+        'hr_as_of', 
+        'ic', 
+        'ic_as_of', 
+        'current_survey_sections', 
+        'prior_survey_sections', 
+        'prior_2_survey_sections'])
+        
+    survey_dictionary = get_updated_dictionary(survey_info_in, default_dictionary)
+        
+    return survey_dictionary
