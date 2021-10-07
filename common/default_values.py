2b86ab694e6e43fe2786f49efc6eeefee604f78e
jhanicak <31990830+jhanicak@users.noreply.github.com>
2021-10-07 18:13:08 -0500

    Update default_values.py

--- common/default_values.py
+++ common/default_values.py
@@ -5,13 +5,19 @@ import json
 from uuid import uuid4
 from pyspark.sql.window import Window
 from pyspark.sql.functions import sum as sum, expr, col, lit, upper, to_timestamp, max, min, row_number, date_trunc, \
-    to_date, when, coalesce, count, rank
+    to_date, when, coalesce, count, rank, concat, substring
 from pyspark.sql.utils import AnalysisException
 from datetime import datetime
 from pyspark import SparkContext
 from awsglue.context import GlueContext
 from pyspark.sql import SQLContext, types as T, functions as f, SparkSession
 from awsglue.utils import getResolvedOptions
+
+#***************************************************************
+#*
+#***  get_12me_dict 
+#*
+#*************************************************************** 
 
 def get_12me_dict(survey_ver_id, year_in, default_dictionary_in):
     
@@ -28,13 +34,19 @@ def get_12me_dict(survey_ver_id, year_in, default_dictionary_in):
     default_dictionary_in['report_end'] = datetime.strftime(datetime.strptime((year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
     default_dictionary_in['cohort_academic_fall'] = 'Fall Census'
     default_dictionary_in['cohort_academic_pre_fall_summer'] = 'Pre-Fall Summer Census'
-    default_dictionary_in['cohort_academic_spring_tag'] = 'Spring Census'
-    default_dictionary_in['cohort_academic_post_spring_summer_tag'] = 'Post-Spring Summer Census'
+    default_dictionary_in['cohort_academic_spring'] = 'Spring Census'
+    default_dictionary_in['cohort_academic_post_spring_summer'] = 'Post-Spring Summer Census'
     default_dictionary_in['cohort_program'] = 'Academic Year End'
     default_dictionary_in['cohort_program_2'] = 'June End'
     default_dictionary_in['current_survey_sections'] = ['COHORT']
         
     return default_dictionary_in
+
+#***************************************************************
+#*
+#***  get_gr_dict 
+#*
+#*************************************************************** 
 
 def get_gr_dict(survey_ver_id, year_in, default_dictionary_in):
 
@@ -68,6 +80,7 @@ def get_gr_dict(survey_ver_id, year_in, default_dictionary_in):
         default_dictionary_in['earliest_or_only_status_as_of'] = datetime.strftime(datetime.strptime((prior_year_2 + '-08-30'), "%Y-%m-%d"), "%Y-%m-%d")
         default_dictionary_in['mid_or_only_status'] = 'August End'
         default_dictionary_in['mid_or_only_status_as_of'] = datetime.strftime(datetime.strptime((prior_year + '-08-30'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['financial_aid_end'] = datetime.strftime(datetime.strptime((gr_4yr_cohort_next_year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
 
     elif survey_ver_id == 'GR2':        
         default_dictionary_in['cohort_program'] = 'August End'
@@ -77,20 +90,28 @@ def get_gr_dict(survey_ver_id, year_in, default_dictionary_in):
         default_dictionary_in['earliest_or_only_status_as_of'] = datetime.strftime(datetime.strptime((prior_year_2 + '-08-30'), "%Y-%m-%d"), "%Y-%m-%d")
         default_dictionary_in['mid_or_only_status'] = 'August End'
         default_dictionary_in['mid_or_only_status_as_of'] = datetime.strftime(datetime.strptime((prior_year + '-08-30'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['financial_aid_end'] = datetime.strftime(datetime.strptime((gr_4yr_cohort_next_year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
         
     elif survey_ver_id in ('GR3', 'GR5'):
         default_dictionary_in['cohort_academic_fall'] = 'Fall Census'
         default_dictionary_in['default_fall_census'] = datetime.strftime(datetime.strptime((gr_2yr_cohort_year + '-10-15'), "%Y-%m-%d"), "%Y-%m-%d")
         default_dictionary_in['cohort_academic_pre_fall_summer'] = 'Pre-Fall Summer Census'
+        default_dictionary_in['financial_aid_end'] = datetime.strftime(datetime.strptime((gr_2yr_cohort_next_year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
         
     else: # survey_ver_id in ('GR4', 'GR6'):        
         default_dictionary_in['cohort_program'] = 'August End'
         default_dictionary_in['cohort_program_start'] = datetime.strftime(datetime.strptime((gr_2yr_cohort_year + '-09-01'), "%Y-%m-%d"), "%Y-%m-%d")
         default_dictionary_in['cohort_program_end'] = datetime.strftime(datetime.strptime((gr_2yr_cohort_next_year + '-08-31'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['financial_aid_end'] = datetime.strftime(datetime.strptime((gr_2yr_cohort_next_year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
         
     return default_dictionary_in
     
-    
+#***************************************************************
+#*
+#***  get_sfa_dict 
+#*
+#*************************************************************** 
+
 def get_sfa_dict(survey_ver_id, year_in, default_dictionary_in):
 
 #SFA1 Student Financial Aid for public institutions reporting on a fall cohort (academic reporters)
@@ -104,16 +125,21 @@ def get_sfa_dict(survey_ver_id, year_in, default_dictionary_in):
     prior_year = str(int(year) - 1) 
     
     default_dictionary_in['financial_aid'] = 'Financial Aid Year End'
+    default_dictionary_in['financial_aid_end'] = datetime.strftime(datetime.strptime((year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
     default_dictionary_in['gi_bill'] = 'GI Bill'
     default_dictionary_in['gi_bill_start'] = datetime.strftime(datetime.strptime((prior_year + '-07-01'), "%Y-%m-%d"), "%Y-%m-%d")
     default_dictionary_in['gi_bill_end'] = datetime.strftime(datetime.strptime((year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
     default_dictionary_in['department_of_defense'] = 'Department of Defense'
     default_dictionary_in['department_of_defense_start'] = datetime.strftime(datetime.strptime((prior_year + '-10-01'), "%Y-%m-%d"), "%Y-%m-%d")
-    default_dictionary_in['department_of_defense_end'] = datetime.strftime(datetime.strptime((year + '-09-30'), "%Y-%m-%d"), "%Y-%m-%d")
-    default_dictionary_in['current_survey_sections'] = ['COHORT', 'PRIOR SUMMER']   #['FALL', 'COHORT', 'PRIOR SUMMER']  
+    default_dictionary_in['department_of_defense_end'] = datetime.strftime(datetime.strptime((year + '-09-30'), "%Y-%m-%d"), "%Y-%m-%d")    
+    default_dictionary_in['current_survey_sections'] = ['COHORT', 'PRIOR SUMMER']
     default_dictionary_in['prior_survey_sections'] = ['PRIOR YEAR 1 COHORT', 'PRIOR YEAR 1 PRIOR SUMMER']
     default_dictionary_in['prior_2_survey_sections'] = ['PRIOR YEAR 2 COHORT', 'PRIOR YEAR 2 PRIOR SUMMER']
-    
+####****TEST uncomment next 3 lines for test data '1415'
+    #default_dictionary_in['current_survey_sections'] = ['FALL', 'COHORT', 'PRIOR SUMMER']  
+    #default_dictionary_in['prior_survey_sections'] = ['PRIOR YEAR 1 FALL', 'PRIOR YEAR 1 COHORT', 'PRIOR YEAR 1 PRIOR SUMMER']
+    #default_dictionary_in['prior_2_survey_sections'] = ['PRIOR YEAR 2 FALL', 'PRIOR YEAR 2 COHORT', 'PRIOR YEAR 2 PRIOR SUMMER']
+  
     if survey_ver_id in ('SFA1', 'SFA2'):
         default_dictionary_in['cohort_academic_fall'] = 'Fall Census'
         default_dictionary_in['default_fall_census'] = datetime.strftime(datetime.strptime((prior_year + '-10-15'), "%Y-%m-%d"), "%Y-%m-%d")
@@ -123,8 +149,7 @@ def get_sfa_dict(survey_ver_id, year_in, default_dictionary_in):
         default_dictionary_in['cohort_hybrid_end'] = datetime.strftime(datetime.strptime((prior_year + '-10-31'), "%Y-%m-%d"), "%Y-%m-%d")
         
     elif survey_ver_id in ('SFA3', 'SFA4'):
-        default_dictionary_in['financial_aid_start'] = datetime.strftime(datetime.strptime((year + '-07-01'), "%Y-%m-%d"), "%Y-%m-%d")
-        default_dictionary_in['financial_aid_end'] = datetime.strftime(datetime.strptime((next_year + '-06-30'), "%Y-%m-%d"), "%Y-%m-%d")
+        default_dictionary_in['financial_aid_start'] = datetime.strftime(datetime.strptime((prior_year + '-07-01'), "%Y-%m-%d"), "%Y-%m-%d")
         default_dictionary_in['cohort_program'] = 'Academic Year End'
         default_dictionary_in['cohort_program_start'] = datetime.strftime(datetime.strptime((prior_year + '-10-01'), "%Y-%m-%d"), "%Y-%m-%d")
         default_dictionary_in['cohort_program_end'] = datetime.strftime(datetime.strptime((prior_year + '-10-31'), "%Y-%m-%d"), "%Y-%m-%d")
@@ -134,7 +159,12 @@ def get_sfa_dict(survey_ver_id, year_in, default_dictionary_in):
         
     return default_dictionary_in
 
-    
+#***************************************************************
+#*
+#***  get_updated_dictionary 
+#*
+#***************************************************************
+
 def get_updated_dictionary(survey_info_in, default_dictionary_in):
     
     survey_type = survey_info_in['survey_type']
@@ -145,59 +175,58 @@ def get_updated_dictionary(survey_info_in, default_dictionary_in):
         survey_default_values = get_sfa_dict(survey_ver_id, year, default_dictionary_in)
     elif survey_type == '12ME':
         survey_default_values = get_12me_dict(survey_ver_id, year, default_dictionary_in)
-    else: # survey_type == 'GR':
+    elif survey_type == 'GR':
         survey_default_values = get_gr_dict(survey_ver_id, year, default_dictionary_in)
         
     return survey_default_values
     
-    
 #***************************************************************
 #*
-#***  get_survey_tags 
+#***  get_survey_default_values 
 #*
 #***************************************************************
 
 def get_survey_default_values(survey_info_in):
 
-    default_dictionary = dict.fromkeys(['report_start',
-        'report_end',
-        'cohort_academic_fall',
-        'default_fall_census',
-        'cohort_academic_pre_fall_summer',
-        'cohort_academic_spring',
-        'cohort_academic_post_spring_summer',
-        'cohort_hybrid',
-        'cohort_hybrid_start',
-        'cohort_hybrid_end',
-        'cohort_program',
-        'cohort_program_2', 
-        'cohort_program_start',
-        'cohort_program_end', 
-        'gi_bill',
-        'gi_bill_start', 
-        'gi_bill_end',
-        'department_of_defense', 
-        'department_of_defense_start', 
-        'department_of_defense_end', 
-        'financial_aid', 
-        'financial_aid_start', 
-        'financial_aid_end', 
-        'transfer',
-        'earliest_status', 
-        'earliest_status_as_of', 
-        'mid_status', 
-        'mid_status_as_of', 
-        'latest_or_only_status', 
-        'latest_or_only_status_as_of', 
-        'fiscal_year', 
-        'fiscal_year_as_of', 
-        'hr', 
-        'hr_as_of', 
-        'ic', 
-        'ic_as_of', 
-        'current_survey_sections', 
-        'prior_survey_sections', 
-        'prior_2_survey_sections'])
+    default_dictionary = {'report_start' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"),
+        'report_end' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"),
+        'cohort_academic_fall' : 'x',
+        'default_fall_census' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"),
+        'cohort_academic_pre_fall_summer' : 'x',
+        'cohort_academic_spring' : 'x',
+        'cohort_academic_post_spring_summer' : 'x',
+        'cohort_hybrid' : 'x',
+        'cohort_hybrid_start' : datetime.strftime(datetime.strptime('1900-09-09', "%Y-%m-%d"), "%Y-%m-%d"),
+        'cohort_hybrid_end' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"),
+        'cohort_program' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"),
+        'cohort_program_2' : 'x', 
+        'cohort_program_start' : datetime.strftime(datetime.strptime('1900-09-09', "%Y-%m-%d"), "%Y-%m-%d"),
+        'cohort_program_end' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'gi_bill' : 'x',
+        'gi_bill_start' : datetime.strftime(datetime.strptime('1900-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'gi_bill_end' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"),
+        'department_of_defense' : 'x', 
+        'department_of_defense_start' : datetime.strftime(datetime.strptime('1900-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'department_of_defense_end' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'financial_aid' : 'x', 
+        'financial_aid_start' : datetime.strftime(datetime.strptime('1900-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'financial_aid_end' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'transfer' : 'x',
+        'earliest_status' : 'x', 
+        'earliest_status_as_of' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'mid_status' : 'x', 
+        'mid_status_as_of' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'latest_or_only_status' : 'x', 
+        'latest_or_only_status_as_of' :  datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'fiscal_year' : 'x', 
+        'fiscal_year_as_of' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'hr' : 'x', 
+        'hr_as_of' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'ic' : 'x', 
+        'ic_as_of' : datetime.strftime(datetime.strptime('9999-09-09', "%Y-%m-%d"), "%Y-%m-%d"), 
+        'current_survey_sections' : 'x', 
+        'prior_survey_sections' : 'x', 
+        'prior_2_survey_sections' : 'x'}
         
     survey_dictionary = get_updated_dictionary(survey_info_in, default_dictionary)
         
